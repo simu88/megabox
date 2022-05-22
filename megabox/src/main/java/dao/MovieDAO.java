bb3a49f2ca410dd575f9d7ebe6ad833e8c0f0e51
kimdotcom2 <105586594+kimdotcom2@users.noreply.github.com>
2022-05-22 23:39:23 +0900

    Update MovieDAO.java

--- megabox/src/main/java/dao/MovieDAO.java
+++ megabox/src/main/java/dao/MovieDAO.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
 
 import common.DBConfig;
 import model.*;
@@ -13,6 +15,14 @@ public class MovieDAO {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
+		Date date = new Date(0);
+		
+		SimpleDateFormat simpledate = new SimpleDateFormat(movieDTO.getRelease_date() + "");
+		
+		String formatedDate = simpledate.format(date);
+		
+		java.sql.Date releaseDate = java.sql.Date.valueOf(formatedDate);
+		
 		
 		try {
 			Class.forName(DBConfig.driver);
@@ -21,33 +31,40 @@ public class MovieDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch(SQLException e){
-            System.out.println("ø°∑Ø: " + e);
+            System.out.println("ÏóêÎü¨: " + e);
         }
 		
-		//DAO ∞¯≈Î ƒ⁄µÂ
+		//DAO Í≥µÌÜµ ÏΩîÎìú
 		
 		String sql = "INSERT INTO movie VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		
-		pstmt = con.prepareStatement(sql);
+		try {
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, movieDTO.getTitle());
+			pstmt.setString(2, movieDTO.getTitle_origin());
+			pstmt.setString(3, movieDTO.getThumbnail_image());
+			pstmt.setDate(4, releaseDate);
+			pstmt.setString(5, movieDTO.getSummary());
+			pstmt.setString(6, movieDTO.getType());
+			pstmt.setString(7, movieDTO.getDirector());
+			pstmt.setString(8, movieDTO.getGenre());
+			pstmt.setInt(9, movieDTO.getRating());
+			pstmt.setString(10, movieDTO.getCast());
+			pstmt.setString(11, movieDTO.getPreview_url());
+			pstmt.setInt(12, movieDTO.getReview_count());
+			pstmt.setInt(14, movieDTO.getSum_score());
+			
+			pstmt.executeUpdate();
+			
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		pstmt.setString(1, movieDTO.getTitle());
-		pstmt.setString(2, movieDTO.getTitle_origin());
-		pstmt.setString(3, movieDTO.getThumbnail_image());
-		pstmt.setDate(4, movieDTO.getRelease_date());
-		pstmt.setString(5, movieDTO.getSummary());
-		pstmt.setString(6, movieDTO.getType());
-		pstmt.setString(7, movieDTO.getDirector());
-		pstmt.setString(8, movieDTO.getGenre());
-		pstmt.setInt(9, movieDTO.getRating());
-		pstmt.setString(10, movieDTO.getCast());
-		pstmt.setString(11, movieDTO.getPreview_url());
-		pstmt.setInt(12, movieDTO.getReview_count());
-		pstmt.setInt(14, movieDTO.getSum_score());
 		
-		pstmt.executeUpdate();
-		
-		pstmt.close();
-		con.close();
 		
 	}
 }
