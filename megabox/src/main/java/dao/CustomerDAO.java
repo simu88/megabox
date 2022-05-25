e2fe66174ddc265d8b5ab0a539c665de6300adbb
kimdotcom2 <105586594+kimdotcom2@users.noreply.github.com>
2022-05-25 23:49:06 +0900

    Add files via upload
    //CustomerDAO 추가

---
+++ megabox/src/main/java/dao/CustomerDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import common.DBConfig;
+
+public class CustomerDAO {
+	//가입과 권한 조회를 제외한 이용자와 관련된 DAO
+	public static ResultSet checkID(String id) {
+		
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			try {
+				Class.forName(DBConfig.driver);
+				con = DriverManager.getConnection(DBConfig.URL, DBConfig.dbUserName, DBConfig.dbPassword);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch(SQLException e){
+	            System.out.println("에러: " + e);
+	        }
+			
+			String sql = "SELECT COUNT(*) FROM customer where id =?";
+			try {
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, id);
+				rs = pstmt.executeQuery();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			return rs;
+			
+	}
+
+}
