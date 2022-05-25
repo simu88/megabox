1ffb66d5246f13be019cdc58609fa3d0d418db86
kimdotcom2 <105586594+kimdotcom2@users.noreply.github.com>
2022-05-25 22:42:55 +0900

    Update Test.java

--- megabox/src/main/java/service/Test.java
+++ megabox/src/main/java/service/Test.java
@@ -7,14 +7,9 @@ import dao.SignUpDAO;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			SignUpDAO.register("a", "b", "c", "d");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws SQLException {
+		SignUpService.signUp("a", "b", "c", "d");
 	}
 
 }
+
