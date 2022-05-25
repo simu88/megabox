417126d03262e931204f0bfdfd014a13ac691b73
kimdotcom2 <105586594+kimdotcom2@users.noreply.github.com>
2022-05-25 23:51:06 +0900

    Add files via upload

---
+++ megabox/src/main/java/model/ReserveDTO.java
@@ -0,0 +1,95 @@
+package model;
+
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class ReserveDTO {
+
+	int customer_id;
+	int schedule_id;
+	Date date;
+	String creditcard;
+	String payment_type;
+	LocalDateTime reserve_time;
+	int customer_count;
+	int price_sum;
+
+	public ReserveDTO(int customer_id, int schedule_id, Date date, String creditcard, String payment_type,
+			LocalDateTime reserve_time, int customer_count, int price_sum) 
+	{
+		this.customer_id=customer_id;
+		this.schedule_id=schedule_id;
+		this.date=date;
+		this.creditcard=creditcard;
+		this.payment_type=payment_type;
+		this.reserve_time=reserve_time;
+		this.customer_count=customer_count;
+		this.price_sum=price_sum;
+
+	}
+
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id=customer_id;
+	}
+	
+	public int getSchedule_id() {
+		return schedule_id;
+	}
+
+	public void setSchedule_id(int schedule_id) {
+		this.schedule_id=schedule_id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date=date;
+	}
+	
+	public String getCreditcard() {
+		return creditcard;
+	}
+
+	public void setCreditcard(String creditcard) {
+		this.creditcard=creditcard;
+	}
+	
+	public String getPayment_type() {
+		return payment_type;
+	}
+
+	public void setPayment_type(String payment_type) {
+		this.payment_type=payment_type;
+	}
+	
+	public LocalDateTime getReserve_time() {
+		return reserve_time;
+	}
+
+	public void setReserve_time(LocalDateTime reserve_time) {
+		this.reserve_time=reserve_time;
+	}
+	
+	public int getCustomer_count() {
+		return customer_count;
+	}
+
+	public void setCustomer_count(int customer_count) {
+		this.customer_count=customer_count;
+	}
+	
+	public int getPrice_sum() {
+		return price_sum;
+	}
+
+	public void setPrice_sum(int price_sum) {
+		this.price_sum=price_sum;
+	}
+}
---
+++ megabox/src/main/java/model/Schedule.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.time.LocalTime;
+import java.util.*;
+
+public class Schedule {
+
+	int movie_id;
+	int auditorium_id;
+	Date date;
+	LocalTime time;
+	LocalTime end_time;
+	int rest_seat_count;
+
+	public Schedule(int movie_id, int auditorium_id, Date date, LocalTime time, LocalTime end_time, int rest_seat_count) {
+
+		this.movie_id=movie_id;
+		this.auditorium_id=auditorium_id;
+		this.date=date;
+		this.time=time;
+		this.end_time=end_time;
+		this.rest_seat_count=rest_seat_count;
+
+	}
+
+	public int getMovie_id() {
+		return movie_id;
+	}
+
+	public void setMovie_id() {
+		this.movie_id=movie_id;
+	}
+	
+	public int getAuditorium_id() {
+		return auditorium_id;
+	}
+
+	public void setAuditorium_id(int auditorium_id) {
+		this.auditorium_id=auditorium_id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date=date;
+	}
+	
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setLocalTime(LocalTime time) {
+		this.time=time;
+	}
+	
+	public LocalTime getEnd_time() {
+		return end_time;
+	}
+
+	public void setEnd_time(LocalTime end_time) {
+		this.end_time=end_time;
+	}
+	
+	public int getRest_seat_count() {
+		return rest_seat_count;
+	}
+
+	public void setRest_seat_count(int rest_seat_count) {
+		this.rest_seat_count=rest_seat_count;
+	}
+	
+	
+}
